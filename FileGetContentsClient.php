e93ec7101bc0d4471d6d6ba4ca017b1dd86423fa
Patrick Kolencherry <pkolencherry@twilio.com>
2017-01-10 14:26:35 -0600

    WIP - buildOptionsArray. Completed buildQueryString

--- FileGetContentsClient.php
+++ FileGetContentsClient.php
@@ -29,36 +29,57 @@ class FileGetContentsClient implements Client {
     **
     **  @return array Array of options that's consumable by file_get_contents
     */
-    public function buildOptionsArray($method, $url, $params=array(), $data=array(), 
+    public function buildOptionsArray($method, $params=array(), $data=array(), 
         $headers=array(), $username=null, $password=null, $timeout=null){
 
         $options=array();
 
+        $options['headers']=array();
+        // PHP defaults to HTTP 1.0 for HTTP Contexts — Twilio uses 1.1 w/ CurlClient
+        $options['protocol_version']=1.1;
+
         $options['method']=strtoupper(trim($method));
         $options['timeout']=($timeout)?$timeout:self::DEFAULT_HTTP_TIMEOUT;
-        $options['headers']=array();
 
         // Add each header to headers array in Header: Value format
-        foreach($headers as $k=>$v){
+        foreach($headers as $k=>$v)
             $options['headers'][]="$k: $v";
-        }
 
         // Add Authorization header if username AND password are present
-        if($username && $password){
+        if($username && $password)
             $options['headers'][] = 'Authorization: Basic '
                 .base64_encode("$username:$password");
-        }
+
+        
 
         return $options;
     }
 
     /*
-    **  Builds a query string for fields required for a POST request
+    **  Builds a query string for fields, as required by POST bodies and GET URLs
     **  
     **  @return string Query string with all fields appropriately encoded
     */
-    public function buildPostQueryString(){
+    public function buildQueryString($params){
+        // Funny enough, despite calling it queryParts, this will handle multipart
+        $queryParts=array();
 
+        // Takes care of the case of Truthy params as input to function
+        if(!is_array($params))
+            return "";
+
+        foreach($params as $key=>$value){
+            // Duplicate key handling. For paramDupe=array(1,2), Twilio requires 
+            // paramDupe=1&paramDupe=2, instead of paramDupe=1%2C2
+            if(is_array($value)){
+                foreach($value as $dupeParam)
+                    $queryParts[]=urlencode((string)$key).'='
+                        .urlencode((string)$dupeParam);
+            }else
+                $queryParts[]=urlencode((string)$key).'='.urlencode((string)$value);
+        }
+
+        return implode('&', $queryParts);
     }
 }
 
--- spec/FileGetContentsClient.spec.php
+++ spec/FileGetContentsClient.spec.php
@@ -1,11 +1,12 @@
 <?php
+// Yeah, that's right, there's tests for this sample code
 require 'FileGetContentsClient.php';
 use TAHClient\FileGetContentsClient;
-// Yeah, that's right, there's tests
 
 describe('FileGetContentsClient', function(){
     beforeAll(function(){
         $this->client=new FileGetContentsClient();
+        $this->prefix='https://httpbin.org/'
     });
 
     describe('::buildOptionsArray', function(){
@@ -14,17 +15,66 @@ describe('FileGetContentsClient', function(){
                 'X-Patrick-Header' => 'Kolencherry',
                 'X-Forwarded-By' => '1.2.3.4'
             );
-            $res=$this->client->buildOptionsArray(null, null, array(), array(), $headers);
+            $res=$this->client->buildOptionsArray(null, array(), array(), $headers);
             expect($res['headers'])->toContain('X-Patrick-Header: Kolencherry', 
                 'X-Forwarded-By: 1.2.3.4');
         });
 
         it('should correctly generate a basic Auth header', function(){
-            $res=$this->client->buildOptionsArray(null, null, array(), array(), array(), 
+            $res=$this->client->buildOptionsArray(null, array(), array(), array(), 
                 'patrick', 'kolencherry');
 
             expect($res['headers'])->toContain(
                 'Authorization: Basic cGF0cmljazprb2xlbmNoZXJyeQ==');
+        });
+
+        it('should correctly override the default HTTP timeout', function(){
+            $res=$this->client->buildOptionsArray(null, array(), array(), array(),
+                null, null, 120);
+
+            expect($res['timeout'])->toEqual(120);
+        });
+    });
+
+    describe('::buildQueryString', function(){
+        it('should return a Falsy value for an empty array', function(){
+            expect($this->client->buildQueryString(array()))->toBeFalsy();
+        });
+
+        it('should return a Falsy value for an non-array', function(){
+            expect($this->client->buildQueryString(123))->toBeFalsy();
+            expect($this->client->buildQueryString('abd'))->toBeFalsy();
+            expect($this->client->buildQueryString(true))->toBeFalsy();
+        });
+
+        it('should return a urlencoded query string for unsafe characters', function(){
+            $params=array('url_parameter' => '=,/+&"123‰patrick'); // I herd u liek utf-8
+
+            expect($this->client->buildQueryString($params))
+                ->toEqual('url_parameter=%3D%2C%2F%2B%26%22123%E2%80%B0patrick');
+
+            // Handle the test-case of an unsafe parameter name
+            $params=array(',/+&"123‰patrick='=>'url_parameter');
+            expect($this->client->buildQueryString($params))
+                ->toEqual('%2C%2F%2B%26%22123%E2%80%B0patrick%3D=url_parameter');
+        });
+
+        it('should return a query string for params with multiple values', function(){
+            $params=array('url_parameter'=>array('patrick', 'kolencherry'));
+
+            expect($this->client->buildQueryString($params))
+                ->toEqual('url_parameter=patrick&url_parameter=kolencherry');
+        });
+    });
+
+    // ToDo: expand tests to cover more request types
+    describe('::request', function(){
+        it('should return a Response object', function(){
+            
+        })
+
+        it('should make a GET request', function(){
+
         });
     });
 })
